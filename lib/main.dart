819d626312800eaffa69f0982c15cba2a9dbc1aa
Anshul Kapoor <anshulkapoor306@gmail.com>
2024-06-09 23:51:03 +0530

    one part complete

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SecondScreen());
+        home: const HomeScreen());
   }
 }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:restapis/utilities/model_p.dart';
+import 'package:restapis/Models/model_p.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
